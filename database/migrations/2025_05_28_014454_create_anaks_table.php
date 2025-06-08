c00a5f370ab91d564f75efc82eb3179d33133932
lili5777 <zailyanzali@gmail.com>
2025-06-08 21:58:19 +0800

    update

--- database/migrations/2025_05_28_014454_create_anaks_table.php
+++ database/migrations/2025_05_28_014454_create_anaks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('anaks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            // $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('nama');
             $table->foreignId('usia_id')->constrained('usias')->onDelete('cascade');
             $table->float('tb');
